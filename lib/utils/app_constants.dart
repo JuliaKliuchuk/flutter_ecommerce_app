dd5ae2db39c1535ea7a4b7317b510ce360571840
kudrautsava.yuliya <kudrautsava.yuliya@gmail.com>
2023-02-13 19:54:27 +0300

    added new base url/ connct to self db

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -4,7 +4,8 @@ class AppConstans {
   static const String APP_NAME = 'DBFood';
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = 'http://mvs.bslmeiyu.com';
+  // static const String BASE_URL = 'http://mvs.bslmeiyu.com';
+  static const String BASE_URL = 'http://192.168.100.4:8080';
   static const String POPULAR_PRODUCT_URL = '/api/v1/products/popular';
   static const String RECOMMENDED_PRODUCT_URL = '/api/v1/products/recommended';
   static const String UPLOAD_URL = '/uploads/';
